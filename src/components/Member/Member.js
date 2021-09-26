d78491fd867e72c7ac71a37d2956a07b423e3fb7
jhsayem021 <sayemmetro420@gmail.com>
2021-09-26 17:01:07 +0600

    4th Commit

--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faUser } from '@fortawesome/free-solid-svg-icons'
+import {  faUser  } from '@fortawesome/free-solid-svg-icons'
 
 import './Member.css'
 
